a903b3415fd5201e94fbbcc811839f9a6ed7e076
Fernando <fernando.gonzalez@edunext.co>
2023-10-13 20:24:53 -0400

    feat: /api/ora_staff_grader/initialize upgraded

--- openassessment/staffgrader/serializers/submission_list.py
+++ openassessment/staffgrader/serializers/submission_list.py
@@ -2,6 +2,7 @@
 Serializers for the Submission List endpoint
 """
 from rest_framework import serializers
+
 from openassessment.assessment.models.staff import StaffWorkflow
 
 
@@ -11,71 +12,100 @@ class MissingContextException(Exception):
 
 # pylint: disable=abstract-method
 class SubmissionListScoreSerializer(serializers.Serializer):
-    pointsEarned = serializers.IntegerField(source='points_earned')
-    pointsPossible = serializers.IntegerField(source='points_possible')
+    pointsEarned = serializers.IntegerField(source="points_earned")
+    pointsPossible = serializers.IntegerField(source="points_possible")
 
 
 class SubmissionListSerializer(serializers.ModelSerializer):
     """
     Serialized info about an item returned from the submission list endpoint
     """
+
     class Meta:
         model = StaffWorkflow
         fields = [
-            'submissionUuid',
-            'dateSubmitted',
-            'dateGraded',
-            'gradingStatus',
-            'lockStatus',
-            'gradedBy',
-            'username',
-            'teamName',
-            'score'
+            "submissionUuid",
+            "dateSubmitted",
+            "dateGraded",
+            "gradingStatus",
+            "lockStatus",
+            "gradedBy",
+            "username",
+            "teamName",
+            "score",
+            "email",
+            "fullname",
         ]
         read_only_fields = fields
 
     requires_context = True
 
     # Required context
-    CONTEXT_ANON_ID_TO_USERNAME = 'anonymous_id_to_username'
-    CONTEXT_SUB_TO_ASSESSMENT = 'submission_uuid_to_assessment'
-    CONTEXT_SUB_TO_ANON_ID = 'submission_uuid_to_student_id'
+    CONTEXT_ANON_ID_TO_USERNAME = "anonymous_id_to_username"
+    CONTEXT_SUB_TO_ASSESSMENT = "submission_uuid_to_assessment"
+    CONTEXT_SUB_TO_ANON_ID = "submission_uuid_to_student_id"
+    CONTEXT_ANON_ID_TO_EMAIL = "anonymous_id_to_email"
+    CONTEXT_ANON_ID_TO_FULLNAME = "anonymous_id_to_fullname"
 
     def _verify_required_context(self, context):
         """Verify that required individual or team context is present for serialization"""
         context_keys = set(context.keys())
 
         # Required context for individual submissions
-        required_context = set([
-            self.CONTEXT_ANON_ID_TO_USERNAME,
-            self.CONTEXT_SUB_TO_ASSESSMENT,
-            self.CONTEXT_SUB_TO_ANON_ID
-        ])
+        required_context = set(
+            [
+                self.CONTEXT_ANON_ID_TO_USERNAME,
+                self.CONTEXT_SUB_TO_ASSESSMENT,
+                self.CONTEXT_SUB_TO_ANON_ID,
+                self.CONTEXT_ANON_ID_TO_EMAIL,
+                self.CONTEXT_ANON_ID_TO_FULLNAME,
+            ]
+        )
 
         missing_context = required_context - context_keys
         if missing_context:
             raise ValueError(f"Missing required context {' ,'.join(missing_context)}")
 
     def __init__(self, *args, **kwargs):
-        self._verify_required_context(kwargs.get('context', {}))
+        self._verify_required_context(kwargs.get("context", {}))
         super().__init__(*args, **kwargs)
 
-    submissionUuid = serializers.CharField(source='submission_uuid')
-    dateSubmitted = serializers.CharField(source='created_at')
-    dateGraded = serializers.CharField(source='grading_completed_at')
+    submissionUuid = serializers.CharField(source="submission_uuid")
+    dateSubmitted = serializers.CharField(source="created_at")
+    dateGraded = serializers.CharField(source="grading_completed_at")
     dateGraded = serializers.SerializerMethodField()
-    gradingStatus = serializers.CharField(source='grading_status')
-    lockStatus = serializers.CharField(source='lock_status')
+    gradingStatus = serializers.CharField(source="grading_status")
+    lockStatus = serializers.CharField(source="lock_status")
     gradedBy = serializers.SerializerMethodField()
     username = serializers.SerializerMethodField()
     teamName = serializers.SerializerMethodField()
     score = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
+    fullname = serializers.SerializerMethodField()
 
     def _get_username_from_context(self, anonymous_user_id):
         try:
             return self.context[self.CONTEXT_ANON_ID_TO_USERNAME][anonymous_user_id]
         except KeyError as e:
-            raise MissingContextException(f"Username not found for anonymous user id {anonymous_user_id}") from e
+            raise MissingContextException(
+                f"Username not found for anonymous user id {anonymous_user_id}"
+            ) from e
+
+    def _get_email_from_context(self, anonymous_user_id):
+        try:
+            return self.context[self.CONTEXT_ANON_ID_TO_EMAIL][anonymous_user_id]
+        except KeyError as e:
+            raise MissingContextException(
+                f"Email not found for anonymous user id {anonymous_user_id}"
+            ) from e
+
+    def _get_fullname_from_context(self, anonymous_user_id):
+        try:
+            return self.context[self.CONTEXT_ANON_ID_TO_FULLNAME][anonymous_user_id]
+        except KeyError as e:
+            raise MissingContextException(
+                f"fullname not found for anonymous user id {anonymous_user_id}"
+            ) from e
 
     def _get_anonymous_id_from_context(self, submission_uuid):
         try:
@@ -104,11 +134,23 @@ class SubmissionListSerializer(serializers.ModelSerializer):
         return None
 
     def get_score(self, workflow):
-        assessment = self.context[self.CONTEXT_SUB_TO_ASSESSMENT].get(workflow.identifying_uuid)
+        assessment = self.context[self.CONTEXT_SUB_TO_ASSESSMENT].get(
+            workflow.identifying_uuid
+        )
         if assessment:
             return SubmissionListScoreSerializer(assessment).data
         else:
             return {}
+
+    def get_email(self, workflow):
+        return self._get_email_from_context(
+            self._get_anonymous_id_from_context(workflow.identifying_uuid)
+        )
+
+    def get_fullname(self, workflow):
+        return self._get_fullname_from_context(
+            self._get_anonymous_id_from_context(workflow.identifying_uuid)
+        )
 
 
 class TeamSubmissionListSerializer(SubmissionListSerializer):
@@ -116,19 +158,23 @@ class TeamSubmissionListSerializer(SubmissionListSerializer):
     Serialized info about an team item returned from the submission list endpoint
     """
 
-    submissionUuid = serializers.CharField(source='team_submission_uuid')
+    submissionUuid = serializers.CharField(source="team_submission_uuid")
 
     # Context keys
-    CONTEXT_ANON_ID_TO_USERNAME = 'anonymous_id_to_username'
-    CONTEXT_SUB_TO_ASSESSMENT = 'submission_uuid_to_assessment'
-    CONTEXT_SUB_TO_TEAM_ID = 'team_submission_uuid_to_team_id'
-    CONTEXT_TEAM_ID_TO_TEAM_NAME = 'team_id_to_team_name'
+    CONTEXT_ANON_ID_TO_USERNAME = "anonymous_id_to_username"
+    CONTEXT_SUB_TO_ASSESSMENT = "submission_uuid_to_assessment"
+    CONTEXT_SUB_TO_TEAM_ID = "team_submission_uuid_to_team_id"
+    CONTEXT_TEAM_ID_TO_TEAM_NAME = "team_id_to_team_name"
+    CONTEXT_ANON_ID_TO_EMAIL = "anonymous_id_to_email"
+    CONTEXT_ANON_ID_TO_FULLNAME = "anonymous_id_to_fullname"
 
     REQUIRED_CONTEXT_KEYS = [
         CONTEXT_ANON_ID_TO_USERNAME,
         CONTEXT_SUB_TO_ASSESSMENT,
         CONTEXT_SUB_TO_TEAM_ID,
         CONTEXT_TEAM_ID_TO_TEAM_NAME,
+        CONTEXT_ANON_ID_TO_EMAIL,
+        CONTEXT_ANON_ID_TO_FULLNAME,
     ]
 
     def _verify_required_context(self, context):
@@ -146,7 +192,9 @@ class TeamSubmissionListSerializer(SubmissionListSerializer):
         try:
             return self.context[self.CONTEXT_TEAM_ID_TO_TEAM_NAME][team_id]
         except KeyError as e:
-            raise MissingContextException(f"Team name not found for team id {team_id}") from e
+            raise MissingContextException(
+                f"Team name not found for team id {team_id}"
+            ) from e
 
     def _get_team_id_from_context(self, team_submission_uuid):
         try:
@@ -157,6 +205,14 @@ class TeamSubmissionListSerializer(SubmissionListSerializer):
             ) from e
 
     def get_username(self, workflow):  # pylint: disable=unused-argument
+        # For team submissions, this is intentionally empty
+        return None
+
+    def get_email(self, workflow):  # pylint: disable=unused-argument
+        # For team submissions, this is intentionally empty
+        return None
+
+    def get_fullname(self, workflow):  # pylint: disable=unused-argument
         # For team submissions, this is intentionally empty
         return None
 
